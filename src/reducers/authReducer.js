bc5e136c35c2adf509ab86cbd9926cb235bfd7d6
Donald Whitely <dswhitely1@gmail.com>
2019-06-24 15:12:45 -0400

    Auth Actions, Types, and Reducer created

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,1 +1,27 @@
-/* BackEnd */
+import types from '../actions';
+
+const initialState = {
+    user: {},
+    isLoading: false,
+    errors: null
+};
+
+export default (state = initialState, action) => {
+    const {type, payload} = action;
+    switch (type) {
+        case types.LOGIN_START:
+            return {...state, isLoading: true};
+        case types.LOGIN_SUCCESS:
+            return {...state, isLoading: false};
+        case types.LOGIN_FAIL:
+            return {...state, isLoading: false};
+        case types.CREATE_USER_START:
+            return {...state, isLoading: true};
+        case types.CREATE_USER_SUCCESS:
+            return {...state, isLoading: false};
+        case types.CREATE_USER_FAIL:
+            return {...state, isLoading: false};
+        default:
+            return state;
+    }
+}
