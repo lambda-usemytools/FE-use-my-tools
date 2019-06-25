40d77027d6a0606258ea6f704051aed69715c973
Donald Whitely <dswhitely1@gmail.com>
2019-06-25 01:08:59 -0400

    Integrating Redux Form to Create Account

--- src/components/auth/CreateAccount.jsx
+++ src/components/auth/CreateAccount.jsx
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
+import {Field,reduxForm} from 'redux-form';
 import {connect} from 'react-redux';
 import {doCreateAccount} from "../../actions/authActions";
 import HeroImage from '../common/HeroImage';
 import userImage from '../../imgs/Oval Copy 3.png';
+import TextInput from "../common/form/TextInput";
 
 class CreateAccount extends Component {
     state = {
@@ -14,36 +16,27 @@ class CreateAccount extends Component {
     };
     onHandleChange = e => this.setState({...this.state, [e.target.name]: e.target.value});
 
-     onHandleSubmit = async e => {
-        console.log(this.props);
-        const {first_name,last_name,email,password} = this.state;
+    onHandleSubmit = async (e)=> {
+        console.log(e );
         e.preventDefault();
         // TODO: Need Validation of password before calling out to DB
-        await this.props.doCreateAccount({first_name,last_name,email,password});
+        // await this.props.doCreateAccount({first_name, last_name, email, password});
+        // this.props.isAuth && this.props.history.push('/dashboard');
     };
 
     render() {
+        const { handleSubmit, pristine, submitting} = this.props;
         return (
             <>
                 <HeroImage/>
-                <form onSubmit={this.onHandleSubmit}>
+                <form onSubmit={e=>handleSubmit(this.onHandleSubmit(e))}>
                     <h4>Create New Account</h4>
-                    <label>First Name</label>
-                    <input name='first_name' value={this.state.first_name} placeholder='First Name'
-                           onChange={this.onHandleChange}/>
-                    <label>Last Name</label>
-                    <input name='last_name' value={this.state.last_name} placeholder='Last Name'
-                           onChange={this.onHandleChange}/>
-                    <label>Email Address</label>
-                    <input name='email' type='email' value={this.state.email} placeholder='Email Address'
-                           onChange={this.onHandleChange}/>
-                    <label>Password</label>
-                    <input name='password' type='password' value={this.state.password} placeholder='Password'
-                           onChange={this.onHandleChange}/>
-                    <label>Confirm Password</label>
-                    <input name='password2' type='password' value={this.state.password2} placeholder='Confirm Password'
-                           onChange={this.onHandleChange}/>
-                    <button type='submit'>Create Account</button>
+                    <Field name='first_name' type='text' component={TextInput} label="First Name" />
+                    <Field name='last_name' type='text' component={TextInput} label="Last Name" />
+                    <Field name='email' type='email' component={TextInput} label="Email Address" />
+                    <Field name='password' type='password' component={TextInput} label="Password" />
+                    <Field name='password2' type='password' component={TextInput} label="Confirm Password" />
+                    <button type='submit' disabled={submitting || pristine}>Create Account</button>
                 </form>
                 <img src={userImage} alt='Happy User'/>
                 <p>I was tired of always loaning tools and not remembering who I lent them to. With Use My Tools I know
@@ -57,4 +50,7 @@ class CreateAccount extends Component {
     }
 }
 
-export default connect(null,{doCreateAccount})(CreateAccount);
+const mapStateToProps = state => ({isAuth: state.auth.isAuth});
+
+CreateAccount = connect(mapStateToProps,{doCreateAccount})(CreateAccount);
+export default reduxForm({form: 'createAccount'})(CreateAccount);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import {reducer as form} from 'redux-form';
 import auth from './authReducer';
 import tools from './toolsReducer';
 
+
 export default combineReducers({
-	auth, tools
+	auth, tools, form
 });
