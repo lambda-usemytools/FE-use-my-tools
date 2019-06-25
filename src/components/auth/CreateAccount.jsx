c7f92e82195dd906efc162dc11c945a8dea5cff0
Donald Whitely <dswhitely1@gmail.com>
2019-06-25 01:50:03 -0400

    Form Validation working on Create Account

--- src/components/auth/CreateAccount.jsx
+++ src/components/auth/CreateAccount.jsx
@@ -1,41 +1,38 @@
 import React, {Component} from 'react';
-import {Field,reduxForm} from 'redux-form';
+import {Field, reduxForm} from 'redux-form';
 import {connect} from 'react-redux';
 import {doCreateAccount} from "../../actions/authActions";
 import HeroImage from '../common/HeroImage';
 import userImage from '../../imgs/Oval Copy 3.png';
 import TextInput from "../common/form/TextInput";
 
-class CreateAccount extends Component {
-    state = {
-        first_name: '',
-        last_name: '',
-        email: '',
-        password: '',
-        password2: ''
-    };
-    onHandleChange = e => this.setState({...this.state, [e.target.name]: e.target.value});
+const required = value => (value ? undefined : 'Required');
+const maxLength = max => value => value && value.length > max ? `Must be ${max} characters or less` : undefined;
+const minLength = min => value => value && value.length < min ? `Must be ${min} characters or more` : undefined;
+const email = value => value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i.test(value) ? 'Invalid Email Address' : undefined;
+const maxLength20 = maxLength(20);
+const minLength2 = minLength(2);
+const minLength5 = minLength(5);
 
-    onHandleSubmit = async (e)=> {
-        console.log(e );
-        e.preventDefault();
-        // TODO: Need Validation of password before calling out to DB
-        // await this.props.doCreateAccount({first_name, last_name, email, password});
-        // this.props.isAuth && this.props.history.push('/dashboard');
-    };
+class CreateAccount extends Component {
 
     render() {
-        const { handleSubmit, pristine, submitting} = this.props;
+        const {handleSubmit, pristine, submitting} = this.props;
         return (
             <>
                 <HeroImage/>
-                <form onSubmit={e=>handleSubmit(this.onHandleSubmit(e))}>
+                <form onSubmit={handleSubmit}>
                     <h4>Create New Account</h4>
-                    <Field name='first_name' type='text' component={TextInput} label="First Name" />
-                    <Field name='last_name' type='text' component={TextInput} label="Last Name" />
-                    <Field name='email' type='email' component={TextInput} label="Email Address" />
-                    <Field name='password' type='password' component={TextInput} label="Password" />
-                    <Field name='password2' type='password' component={TextInput} label="Confirm Password" />
+                    <Field name='first_name' type='text' component={TextInput} label='First Name'
+                           validate={[required, minLength2, maxLength20]}/>
+                    <Field name='last_name' type='text' component={TextInput} label='Last Name'
+                           validate={[required, minLength2, maxLength20]}/>
+                    <Field name='email' type='email' component={TextInput} label='Email Address'
+                           validate={[required, email]}/>
+                    <Field name='password' type='password' component={TextInput} label='Password'
+                           validate={[required, minLength5, maxLength20]}/>
+                    <Field name='password2' type='password' component={TextInput} label='Confirm Password'
+                           validate={[required]}/>
                     <button type='submit' disabled={submitting || pristine}>Create Account</button>
                 </form>
                 <img src={userImage} alt='Happy User'/>
@@ -52,5 +49,5 @@ class CreateAccount extends Component {
 
 const mapStateToProps = state => ({isAuth: state.auth.isAuth});
 
-CreateAccount = connect(mapStateToProps,{doCreateAccount})(CreateAccount);
+CreateAccount = connect(mapStateToProps, {doCreateAccount})(CreateAccount);
 export default reduxForm({form: 'createAccount'})(CreateAccount);
