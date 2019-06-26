bf8e6e3ef5b2f29919b7a7486e196df9eb7aac68
Donald Whitely <dswhitely1@gmail.com>
2019-06-26 15:25:44 -0400

    Working on Header Search for View Tools

--- src/components/tools/viewAllStyle.js
+++ src/components/tools/viewAllStyle.js
@@ -3,17 +3,17 @@ import styled, {css} from 'styled-components';
 export const SearchContainer = styled.div`
   height: 60px;
   width: 501px;
+  margin-top: 25px;  
 `;
 
 export const SearchViewContainer = styled.div`
+  background-color: #EDE3C2;
   display: flex;
-  justify-content: flex-start;
-  align-items: center;
+  justify-content: space-between;
 `;
 
 export const FormSearch = styled.form`
-  display: flex;
-  justify-content: flex-end;
+  display: flex; 
   align-items: flex-end;
   `;
 
@@ -25,4 +25,27 @@ export const SearchButtons = styled.button `
   font-weight: 600;
   color: #488EAC;
   margin-left: 20px;
+  &:hover {
+    background-color: #488EAC;
+    color: #EDE3C2;
+  }
+  ${props => props.actionButton && css`
+    
+    border-radius: 10px;
+    background-color: #488EAC;
+    color: #EDE3C2;
+    height: 48px;
+    font-weight: 600;
+    font-size: 2.4rem;
+    &:hover {
+      background-color: #ede3c2;
+      color: #488EAC;
+    }
+    &:first-child {
+      margin-right: 60px;
+    }
+    &:last-child {
+      margin-right: 87px;
+    }
+`}
 `;
