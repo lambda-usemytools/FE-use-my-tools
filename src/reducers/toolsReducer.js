b9676a6ef03b15dcb430db5958460461af1dad12
Donald Whitely <dswhitely1@gmail.com>
2019-06-25 23:17:04 -0400

    TOOLS Actions and Reducers completed, waiting on BE for 500 errors on POST and PUT routes

--- src/reducers/toolsReducer.js
+++ src/reducers/toolsReducer.js
@@ -10,7 +10,7 @@ const initialState = {
         tool_description: '',
         rental_price: '',
         length_of_rental: '',
-        status: '' //
+        status: ''
     }],
     isLoading: false,
     errors: null
@@ -23,6 +23,31 @@ export default (state = initialState, action) => {
         case types.GET_TOOLS_SUCCESS:
             return {...state, isLoading: false, errors: null, tools: payload};
         case types.GET_TOOLS_FAIL:
+            return {...state, isLoading: false, errors: payload};
+        case types.GET_TOOL_START:
+            return {...state, isLoading: true, errors: null};
+        case types.GET_TOOL_SUCCESS:
+            return {...state, isLoading: false, errors: null, tools: payload};
+        case types.GET_TOOL_FAIL:
+            return {...state, isLoading: false, errors: payload};
+        case types.POST_TOOLS_START:
+            return {...state, isLoading: true, errors: null};
+        case types.POST_TOOLS_SUCCESS:
+            return {...state, isLoading: false, errors: null, tools: payload};
+        case types.POST_TOOLS_FAIL:
+            return {...state, isLoading: false, errors: payload};
+        case types.PUT_TOOL_START:
+            return {...state, isLoading: true, errors: null};
+        case types.PUT_TOOL_SUCCESS:
+            return {...state, isLoading: false, errors: null, tools: payload};
+        case types.PUT_TOOL_FAIL:
+            return {...state, isLoading: false, errors: payload};
+        case types.DELETE_TOOL_START:
+            return {...state, isLoading: true, errors: null};
+        case types.DELETE_TOOL_SUCCESS:
+            const updatedTools = state.tools.filter(tool => tool.id !== payload);
+            return {...state, isLoading: false, errors: null, tools: updatedTools};
+        case types.DELETE_TOOL_FAIL:
             return {...state, isLoading: false, errors: payload};
         default:
             return state;
