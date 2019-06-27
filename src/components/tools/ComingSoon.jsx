b0f790f445eb5409be4cee1813ae148b7f242f41
Wes Jonke <wesjonke@gmail.com>
2019-06-27 12:56:13 -0500

    styled Coming soon page

--- src/components/tools/ComingSoon.jsx
+++ src/components/tools/ComingSoon.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+
+import { BottomContent, Form, FormTop, H4, SubmitButton, Wrapper, TriangleTop,  ButtonContainer, Container, LargeText } from '../tools/ComingSoonStyle';
+import Logo from '../../imgs/favicon-96x96.png';
+
+
 const ComingSoon = props => {
     return (
-        <>
-            <h1>Coming Soon</h1>
-            <button><Link to='/dashboard'>Return Home</Link></button>
-            </>
+        <Wrapper>
+            <FormTop>
+                <TriangleTop>
+                </TriangleTop>
+            </FormTop>
+
+            <BottomContent>
+                <Form>
+                    <Container>
+                        <img src={Logo} alt="use my tools logo"/>
+                        <H4>Coming Soon</H4>
+                        <img src={Logo} alt="use my tools logo"/>
+                    </Container>
+
+                    <ButtonContainer>
+                        <SubmitButton>
+                            <Link to='/dashboard'>Return Home</Link>
+                        </SubmitButton>
+                    </ButtonContainer>
+
+                </Form>
+            </BottomContent>
+
+        </Wrapper>
     )
 }
 
