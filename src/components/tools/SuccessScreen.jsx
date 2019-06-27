003b4825b804a0ddaae8b4420e5340739e5d6303
Wes Jonke <wesjonke@gmail.com>
2019-06-27 11:32:31 -0500

    styled success/error view

--- src/components/tools/SuccessScreen.jsx
+++ src/components/tools/SuccessScreen.jsx
@@ -2,14 +2,34 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux';
 
+import { BottomContent, Form, FormTop, H4, SubmitButton, Wrapper, TriangleTop,  ButtonContainer } from '../tools/SuccessScreenStyle';
+
+
+
 const SuccessScreen = props => {
     return (
-        <>
-            {props.isSuccess ? <h1>Success!  Your tool has been added to My Tools</h1> : <h1>An unknown error has occurred.  Please try your request later</h1>}
-            <button><Link to='/dashboard/add-tool'>Add Another Tool</Link></button>
-            <button><Link to='/dashboard/my-tools'>Go to My Tools</Link></button>
-            <button><Link to='/dashboard'>Go Home</Link></button>
-            </>
+
+        <Wrapper>
+
+        <FormTop>
+            <TriangleTop>            
+            {props.isSuccess ? <H4>Success!  Your tool has been added to My Tools</H4> : <H4>An unknown error has occurred.  Please try your request later</H4>}
+            </TriangleTop>
+        </FormTop>
+
+
+        <BottomContent>
+            <Form>
+                <ButtonContainer>
+                    <SubmitButton><Link to='/dashboard/add-tool'>Add Another Tool</Link></SubmitButton>
+                    <SubmitButton><Link to='/dashboard/my-tools'>Go to My Tools</Link></SubmitButton>
+                    <SubmitButton><Link to='/dashboard'>Go Home</Link></SubmitButton>
+                </ButtonContainer>
+            </Form>
+        </BottomContent>
+
+
+        </Wrapper>
     )
 }
 
