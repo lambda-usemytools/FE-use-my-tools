d31f5dacb7163b66036f7189ae83a43c6e8e8a83
Donald Whitely <dswhitely1@gmail.com>
2019-06-27 09:50:43 -0400

    Status Screen Implemented

--- src/reducers/toolsReducer.js
+++ src/reducers/toolsReducer.js
@@ -16,7 +16,8 @@ const initialState = {
         status: ''
     }],
     isLoading: false,
-    errors: null
+    errors: null,
+    isSuccess: true
 }
 export default (state = initialState, action) => {
     const {type, payload} = action;
@@ -36,15 +37,15 @@ export default (state = initialState, action) => {
         case types.POST_TOOLS_START:
             return {...state, isLoading: true, errors: null};
         case types.POST_TOOLS_SUCCESS:
-            return {...state, isLoading: false, errors: null, tools: payload};
+            return {...state, isLoading: false, errors: null, tools: payload, isSuccess: true};
         case types.POST_TOOLS_FAIL:
-            return {...state, isLoading: false, errors: payload};
+            return {...state, isLoading: false, errors: payload, isSuccess: false};
         case types.PUT_TOOL_START:
             return {...state, isLoading: true, errors: null};
         case types.PUT_TOOL_SUCCESS:
-            return {...state, isLoading: false, errors: null, tools: payload};
+            return {...state, isLoading: false, errors: null, tools: payload, isSuccess: true};
         case types.PUT_TOOL_FAIL:
-            return {...state, isLoading: false, errors: payload};
+            return {...state, isLoading: false, errors: payload, isSuccess: false};
         case types.DELETE_TOOL_START:
             return {...state, isLoading: true, errors: null};
         case types.DELETE_TOOL_SUCCESS:
